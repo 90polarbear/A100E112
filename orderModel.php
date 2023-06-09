02da95135f1cd15271e7cc6d12585f112ee23b91
90polarbear <100282618+90polarbear@users.noreply.github.com>
2023-06-09 22:13:40 +0800

    Update orderModel.php

--- orderModel.php
+++ orderModel.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbuser ='root';//帳號
-$dbpassword = '';//密碼
-$dbname = 'final_exam';
+$dbuser ='sarah';//帳號
+$dbpassword = '00000';//密碼
+$dbname = 'testdb';
 $link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 if($link){
     mysqli_query($link,'SET NAMES utf8');
