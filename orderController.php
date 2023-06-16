065bdc0bc39b995e817618a9e0c5b93001fb9abd
90polarbear <100282618+90polarbear@users.noreply.github.com>
2023-06-16 19:03:07 +0800

    Update orderController.php

--- orderController.php
+++ orderController.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
   $name = $_POST["name"] ?? "";
   if (empty($name)) {
-    $errors[] = "請輸入訂購者姓名";
+    $errors[] = "請輸入訂購者";
   }
 
   $quantity = $_POST["quantity"] ?? "";
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $drink = $_POST["drink"] ?? "";
   $pearl = $_POST['pearl'];
   if ($drink === "soy-milk" && isset($_POST["pearl"])) {
-    $errors[] = "錯誤！豆漿不能加珍珠";
+    $errors[] = "錯誤！期末要歐趴無法加蜂蜜";
   }
 // If there are errors, redirect back to the form with error messages
   if (!empty($errors)) {
@@ -31,9 +31,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   // Calculate total cost
   $total = 0;
   if ($drink === "soy-milk") {
-    $total += 20 * $quantity;
+    $total += 100 * $quantity;
   } elseif ($drink === "milk-tea") {
-    $total += 25 * $quantity;
+    $total += 70 * $quantity;
     if (isset($_POST["pearl"])) {
       $total += 5 * $quantity;
     }
