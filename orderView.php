74928705ce8e438c3ae9dbc99f1682d2ddeda842
90polarbear <100282618+90polarbear@users.noreply.github.com>
2023-06-16 18:59:01 +0800

    Update orderView.php

--- orderView.php
+++ orderView.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>早餐費用試算</title>
+  <title>HeartPanda訂餐試算</title>
   <style>
     .error-message {
       color: red;
@@ -9,23 +9,22 @@
   </style>
 </head>
 <body>
-  <h2>早餐訂購</h2>
+  <h2>訂購</h2>
   <form action="orderController.php" method="POST">
     <div>
-      <label for="name">訂購者姓名：</label>
+      <label for="name">訂購者：</label>
       <input type="text" id="name" name="name" required>
     </div>
     <div>
-      <label>飲料選擇：</label>
       <div>
         <input type="radio" id="soy-milk" name="drink" value="soy-milk" required>
-        <label for="soy-milk">豆漿（$20）</label>
+        <label for="soy-milk">今天笑了梅（$70）</label>
+        <input type="checkbox" id="pearl" name="pearl" value="pearl">
+        <label for="pearl">加蜂蜜（$5）</label>
       </div>
       <div>
         <input type="radio" id="milk-tea" name="drink" value="milk-tea" required>
-        <label for="milk-tea">奶茶（$25）</label>
-        <input type="checkbox" id="pearl" name="pearl" value="pearl">
-        <label for="pearl">珍珠（$5）</label>
+        <label for="milk-tea">期末要歐趴（$100）</label>
       </div>
     </div>
     <div>
